29786bb899e14f02e7895a593dd23ecb42755615
杜宁元 <duny31030@126.com>
2022-07-02 09:34:58 +0800

    更新部分代码

---
+++ Rust_By_Practice/03_variables/04.rs
@@ -0,0 +1,10 @@
+// 修复错误
+fn main() {
+    let x = define_x();
+    println!("{}, world", x);
+}
+
+fn define_x() -> String {
+    let x = "hello".to_string();
+    x
+}
---
+++ Rust_By_Practice/03_variables/07.rs
@@ -0,0 +1,9 @@
+// Sloution 1
+// fn main() {
+//     let _x = 1; 
+// }
+// Sloution 2
+#[allow(unused_variables)]
+fn main() {
+    let x = 1; 
+}
